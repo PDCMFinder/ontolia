882c44ecf05f70bc5a2e10836547f1781ece4634
CsabaHalmagyi <csaba@ebi.ac.uk>
2022-01-20 10:21:30 +0000

    Initial commit

---
+++ src/main/java/org/cancermodels/ontolia/OntoliaApplication.java
@@ -0,0 +1,13 @@
+package org.cancermodels.ontolia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntoliaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OntoliaApplication.class, args);
+    }
+
+}
