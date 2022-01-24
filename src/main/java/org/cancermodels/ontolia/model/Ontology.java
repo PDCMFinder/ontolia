705900f607aaa18637bd0fcd4ec9b8ee9acf4d0c
CsabaHalmagyi <csaba@ebi.ac.uk>
2022-01-24 18:23:28 +0000

    Store regimens that were not linked to treatments

--- src/main/java/org/cancermodels/ontolia/model/Ontology.java
+++ src/main/java/org/cancermodels/ontolia/model/Ontology.java
@@ -1,6 +1,8 @@
 package org.cancermodels.ontolia.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -8,10 +10,14 @@ public class Ontology {
 
     private Map<String, TreatmentOntologyTerm> treatmentTerms;
     private Map<String, RegimenOntologyTerm> regimenTerms;
+    private int linkedRegimenCounter;
+    private List<String> unlinkedRegimensWithReason;
 
     public Ontology() {
         treatmentTerms = new HashMap<>();
         regimenTerms = new HashMap<>();
+        linkedRegimenCounter = 0;
+        unlinkedRegimensWithReason = new ArrayList<>();
     }
 
     public void addTreatmentTerm(TreatmentOntologyTerm term){
@@ -20,5 +26,33 @@ public class Ontology {
 
     public void addRegimenTerm(RegimenOntologyTerm term){
         regimenTerms.put(term.getId(), term);
+    }
+
+    public Map<String, TreatmentOntologyTerm> getTreatmentTerms() {
+        return treatmentTerms;
+    }
+
+    public void setTreatmentTerms(Map<String, TreatmentOntologyTerm> treatmentTerms) {
+        this.treatmentTerms = treatmentTerms;
+    }
+
+    public Map<String, RegimenOntologyTerm> getRegimenTerms() {
+        return regimenTerms;
+    }
+
+    public void setRegimenTerms(Map<String, RegimenOntologyTerm> regimenTerms) {
+        this.regimenTerms = regimenTerms;
+    }
+
+    public int getLinkedRegimenCounter() {
+        return linkedRegimenCounter;
+    }
+
+    public void increaseLinkedRegimenCounter(){
+        this.linkedRegimenCounter += 1;
+    }
+
+    public List<String> getUnlinkedRegimensWithReason() {
+        return unlinkedRegimensWithReason;
     }
 }
