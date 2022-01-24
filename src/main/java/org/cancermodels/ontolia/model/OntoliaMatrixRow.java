b5b12ccea17a40e22c40179f5ab453b544782227
CsabaHalmagyi <csaba@ebi.ac.uk>
2022-01-24 18:30:25 +0000

    Implement ontology linking

---
+++ src/main/java/org/cancermodels/ontolia/model/OntoliaMatrixRow.java
@@ -0,0 +1,104 @@
+package org.cancermodels.ontolia.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OntoliaMatrixRow {
+
+    private String[] synonyms;
+
+    private List<TreatmentOntologyTerm> matchedTerms;
+    private List<String> matchedTermsFoundIn;
+
+    private int matchScore;
+    private boolean completeMatch;
+
+    public OntoliaMatrixRow(String[] synonyms) {
+
+        this.synonyms = synonyms;
+        matchedTerms = new ArrayList<>();
+        matchedTermsFoundIn = new ArrayList<>();
+        matchScore = 0;
+        completeMatch = false;
+    }
+
+
+    /**
+     *
+     * [existingOntologyLabel]
+     * (existingOntologySynonym)
+     * |not found|
+     *
+     * @return
+     */
+    public String getRowString(){
+
+        String s = "";
+        for(int i = 0; i < synonyms.length; i++){
+
+            if(matchedTerms.get(i) != null){
+                if(matchedTermsFoundIn.get(i).equals("label")){
+                    s += "["+matchedTerms.get(i).getLabel()+"] ";
+                }
+                else {
+                    s += "("+matchedTerms.get(i).getLabel()+") ";
+                }
+            }
+            else{
+
+                s += "|"+ synonyms[i]+"| ";
+            }
+        }
+        s += "Score: "+matchScore;
+        return s;
+    }
+
+    public void calculateScore(){
+        for(int i=0; i < matchedTerms.size(); i++){
+            if(matchedTerms.get(i) != null){
+                if(matchedTermsFoundIn.get(i).equals("synonym")) matchScore +=1;
+                if(matchedTermsFoundIn.get(i).equals("label")) matchScore +=10;
+            }
+        }
+    }
+
+    public List<TreatmentOntologyTerm> getMatchedTerms() {
+        return matchedTerms;
+    }
+
+    public void setMatchedTerms(List<TreatmentOntologyTerm> matchedTerms) {
+        this.matchedTerms = matchedTerms;
+    }
+
+    public String[] getSynonyms() {
+        return synonyms;
+    }
+
+    public void setSynonyms(String[] synonyms) {
+        this.synonyms = synonyms;
+    }
+
+    public List<String> getMatchedTermsFoundIn() {
+        return matchedTermsFoundIn;
+    }
+
+    public void setMatchedTermsFoundIn(List<String> matchedTermsFoundIn) {
+        this.matchedTermsFoundIn = matchedTermsFoundIn;
+    }
+
+    public int getMatchScore() {
+        return matchScore;
+    }
+
+    public void setMatchScore(int matchScore) {
+        this.matchScore = matchScore;
+    }
+
+    public boolean isCompleteMatch() {
+        return completeMatch;
+    }
+
+    public void setCompleteMatch(boolean completeMatch) {
+        this.completeMatch = completeMatch;
+    }
+}
---
+++ src/main/java/org/cancermodels/ontolia/services/OntologyLinking.java
@@ -0,0 +1,135 @@
+package org.cancermodels.ontolia.services;
+
+import org.cancermodels.ontolia.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+public class OntologyLinking {
+
+    private Ontology ontology;
+    private Map<String, TreatmentOntologyTerm> treatmentTermLabelsMap;
+    private Map<String, TreatmentOntologyTerm> treatmentTermSynonymsMap;
+    private static final Logger log = LoggerFactory.getLogger(OntologyLinking.class);
+
+    public OntologyLinking(Ontology ontology) {
+        this.ontology = ontology;
+    }
+
+    public void link(){
+        log.info("Linking treatment terms to regimens.");
+        initTermMaps();
+
+        for(Map.Entry<String, RegimenOntologyTerm> entry:ontology.getRegimenTerms().entrySet()){
+            RegimenOntologyTerm regimen = entry.getValue();
+            linkRegimenToTreatments(regimen);
+        }
+        log.info("Linked regimens: "+ontology.getLinkedRegimenCounter());
+        log.info("Linking treatment terms to regimens. DONE");
+
+    }
+
+    private void initTermMaps(){
+        treatmentTermSynonymsMap = new HashMap<>();
+        treatmentTermLabelsMap = new HashMap<>();
+        for(Map.Entry<String, TreatmentOntologyTerm> entry:ontology.getTreatmentTerms().entrySet()){
+            TreatmentOntologyTerm term = entry.getValue();
+            try {
+                treatmentTermLabelsMap.put(term.getLabel().toLowerCase(), term);
+                for (String synonym : term.getSynonyms()) {
+                    treatmentTermSynonymsMap.put(synonym.toLowerCase(), term);
+                }
+            }catch (Exception e){
+                log.error("Exception with"+term);
+            }
+        }
+    }
+
+    private void linkRegimenToTreatments(RegimenOntologyTerm regimen) {
+
+        Set<String> synonyms = regimen.getSynonyms();
+        OntoliaMatrix slashOntoliaMatrix = new OntoliaMatrix("/", treatmentTermLabelsMap, treatmentTermSynonymsMap);
+        OntoliaMatrix dashOntoliaMatrix = new OntoliaMatrix("-", treatmentTermLabelsMap, treatmentTermSynonymsMap);
+
+        for (String synonym : synonyms) {
+
+            String[] synSlashPre = synonym.split("/");
+            String[] synDash = synonym.split("-");
+            String[] synSlash = splitCombos(synSlashPre);
+            slashOntoliaMatrix.addMatrixRow(synSlash);
+            dashOntoliaMatrix.addMatrixRow(synDash);
+        }
+
+        OntoliaMatrixRow slashOMR = slashOntoliaMatrix.getBestCompleteMatch();
+        OntoliaMatrixRow dashOMR = dashOntoliaMatrix.getBestCompleteMatch();
+        OntoliaMatrixRow completeMatch = null;
+
+        if (slashOMR != null && dashOMR != null && slashOMR.getMatchScore() >= dashOMR.getMatchScore())
+            completeMatch = slashOMR;
+        if (slashOMR != null && dashOMR != null && slashOMR.getMatchScore() < dashOMR.getMatchScore())
+            completeMatch = dashOMR;
+        if (slashOMR == null && dashOMR != null) completeMatch = dashOMR;
+        if (slashOMR != null && dashOMR == null) completeMatch = slashOMR;
+
+        if (completeMatch != null) {
+            regimen.setLinkedTreatmentTerms(completeMatch.getMatchedTerms());
+            ontology.increaseLinkedRegimenCounter();
+        } else {
+            //we couldn't match all synonyms to a term
+            String matchString = getBestMatchString(slashOntoliaMatrix, dashOntoliaMatrix);
+            ontology.getUnlinkedRegimensWithReason().add(regimen.getLabel() + " => " + matchString);
+        }
+
+    }
+
+    private String getBestMatchString(OntoliaMatrix slashOntoliaMatrix, OntoliaMatrix dashOntoliaMatrix){
+
+        OntoliaMatrixRow slashOMR = slashOntoliaMatrix.getBestMatch();
+        OntoliaMatrixRow dashOMR = dashOntoliaMatrix.getBestMatch();
+
+        OntoliaMatrixRow bestMatch = null;
+
+        if (slashOMR != null && dashOMR != null && slashOMR.getMatchScore() >= dashOMR.getMatchScore())
+            bestMatch = slashOMR;
+        if (slashOMR != null && dashOMR != null && slashOMR.getMatchScore() < dashOMR.getMatchScore())
+            bestMatch = dashOMR;
+        if (slashOMR == null && dashOMR != null) bestMatch = dashOMR;
+        if (slashOMR != null && dashOMR == null) bestMatch = slashOMR;
+
+        String matchString = "NOT AVAILABLE";
+
+        if (bestMatch != null) {
+            matchString = bestMatch.getRowString();
+        }
+        return matchString;
+    }
+
+    private String[] splitCombos(String[] combos) {
+        //cyclophosphamide followed by paclitaxel + trastuzumab
+        List<String> list = new ArrayList<>();
+
+        for (String c : combos) {
+            if (c.toLowerCase().contains("followed by")) {
+                String[] followedBy = c.toLowerCase().split("followed by");
+                list.add(followedBy[0]);
+                if (followedBy[1].contains("+")) {
+
+                    String[] plusDrugs = followedBy[1].split("\\+");
+                    list.addAll(Arrays.asList(plusDrugs));
+
+                } else if (followedBy[1].contains("/")) {
+
+                    String[] dashDrugs = followedBy[1].split("/");
+                    list.addAll(Arrays.asList(dashDrugs));
+                } else {
+                    list.add(followedBy[1]);
+                }
+            } else {
+                list.add(c);
+            }
+        }
+        return list.stream().toArray(String[]::new);
+    }
+
+}
